420f444e249b49480028fb0cdd7dc452a0415a23
AlexGitHub19 <sasha_tkk@mail.ru>
2019-05-26 13:53:11 +0300

    Image loader is added

---
+++ NotepadShop/NotepadShop/Scripts/Shared/ImageLoader.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('body').on('click', '.loader-btn-contaner', function (e) {
+        $(this).siblings('input').trigger('click');
+    });
+
+    $('body').on('change', '.loader-image-input', function (e) {
+        var jInput = $(this);
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                jInput.siblings('.loader-image-replacement').css('display', 'none');
+                jInput.siblings('.loader-image').attr('src', e.target.result);
+                jInput.siblings('.loader-image').css('display', 'unset');
+            };
+
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+});
