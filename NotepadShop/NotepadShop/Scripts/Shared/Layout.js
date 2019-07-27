6d485573a623ad6963872b3de2dcbb017eabc540
AlexGitHub19 <sasha_tkk@mail.ru>
2019-07-27 21:44:26 +0300

    images are corrected

--- NotepadShop/NotepadShop/Scripts/Shared/Layout.js
+++ NotepadShop/NotepadShop/Scripts/Shared/Layout.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function () {
 
+    $('html').fadeIn();
+
     function createViewModel() {
         var self = this;
         self.email = ko.observable(email == "" ? undefined : email);
