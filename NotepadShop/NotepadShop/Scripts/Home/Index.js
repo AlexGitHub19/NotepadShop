1851c7620ed73a1b4bc4ad7813e94949cc58603a
AlexGitHub19 <sasha_tkk@mail.ru>
2019-08-11 23:37:02 +0300

    Notepad is added to Index page

--- NotepadShop/NotepadShop/Scripts/Home/Index.js
+++ NotepadShop/NotepadShop/Scripts/Home/Index.js
@@ -1,1 +1,19 @@
-﻿
+﻿$(document).ready(function () {
+   $('.notepad-container').css('width', Math.trunc($('.notepad-container').innerWidth()) + 'px');
+});
+
+$('.cover').click(function () {
+    $('body').addClass('open');
+    $('.notepad-container').addClass('open');
+    $('.cover').addClass('open');
+    $('.left-part').addClass('open');
+    $('.right-part').addClass('open');
+});
+
+$('.notepad-close-btn').click(function () {
+    $('body').removeClass('open');
+    $('.cover').removeClass('open');
+    $('.right-part').removeClass('open');
+    $('.left-part').removeClass('open');
+    $('.notepad-container').removeClass('open');
+})
